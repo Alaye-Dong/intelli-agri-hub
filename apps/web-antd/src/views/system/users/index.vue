fa8e4de42365cbeb2e6af577183ac7c4344a7192
Alaye-Dong <AlayeVast@outlook.com>
2024-11-14 20:15:24 +0800

    feat: ✨ 构建用户管理页面基本结构

--- apps/web-antd/src/views/system/users/index.vue
+++ apps/web-antd/src/views/system/users/index.vue
@@ -4,7 +4,7 @@ import type { VxeGridProps } from '#/adapter/vxe-table';
 
 import { Page } from '@vben/common-ui';
 
-import { message } from 'ant-design-vue';
+import { Button, Image, message, Switch, Tag } from 'ant-design-vue';
 
 import { useVbenVxeGrid } from '#/adapter/vxe-table';
 import { getExampleTableApi } from '#/api';
@@ -13,28 +13,42 @@ interface RowType {
   category: string;
   color: string;
   id: string;
+  imageUrl: string;
+  open: boolean;
   price: string;
   productName: string;
   releaseDate: string;
+  status: 'error' | 'success' | 'warning';
 }
 
 const formOptions: VbenFormProps = {
   // 默认展开
   collapsed: false,
+  // 提交函数
+  handleSubmit: onSubmit,
   schema: [
     {
       component: 'Input',
+      componentProps: {
+        placeholder: 'Please enter category',
+      },
       defaultValue: '1',
       fieldName: 'category',
       label: 'Category',
     },
     {
       component: 'Input',
+      componentProps: {
+        placeholder: 'Please enter productName',
+      },
       fieldName: 'productName',
       label: 'ProductName',
     },
     {
       component: 'Input',
+      componentProps: {
+        placeholder: 'Please enter price',
+      },
       fieldName: 'price',
       label: 'Price',
     },
@@ -65,9 +79,18 @@ const formOptions: VbenFormProps = {
   ],
   // 控制表单是否显示折叠按钮
   showCollapseButton: true,
+  submitButtonOptions: {
+    content: '查询',
+  },
   // 按下回车时是否提交表单
   submitOnEnter: false,
 };
+
+function onSubmit(values: Record<string, any>) {
+  message.success({
+    content: `form values: ${JSON.stringify(values)}`,
+  });
+}
 
 const gridOptions: VxeGridProps<RowType> = {
   checkboxConfig: {
@@ -76,24 +99,57 @@ const gridOptions: VxeGridProps<RowType> = {
   },
   columns: [
     { title: '序号', type: 'seq', width: 50 },
-    { align: 'left', title: 'Name', type: 'checkbox', width: 100 },
-    { field: 'category', title: 'Category' },
-    { field: 'color', title: 'Color' },
-    { field: 'productName', title: 'Product Name' },
-    { field: 'price', title: 'Price' },
-    { field: 'releaseDate', formatter: 'formatDateTime', title: 'Date' },
+    { field: 'category', title: 'Category', width: 100 },
+    {
+      field: 'imageUrl',
+      slots: { default: 'image-url' },
+      title: 'Image',
+      width: 100,
+    },
+    {
+      cellRender: { name: 'CellImage' },
+      field: 'imageUrl2',
+      title: 'Render Image',
+      width: 130,
+    },
+    {
+      field: 'open',
+      slots: { default: 'open' },
+      title: 'Open',
+      width: 100,
+    },
+    {
+      field: 'status',
+      slots: { default: 'status' },
+      title: 'Status',
+      width: 100,
+    },
+    { field: 'color', title: 'Color', width: 100 },
+    { field: 'productName', title: 'Product Name', width: 200 },
+    { field: 'price', title: 'Price', width: 100 },
+    {
+      field: 'releaseDate',
+      formatter: 'formatDateTime',
+      title: 'Date',
+      width: 200,
+    },
+    {
+      cellRender: { name: 'CellLink', props: { text: '编辑' } },
+      field: 'action',
+      fixed: 'right',
+      title: '操作',
+      width: 120,
+    },
   ],
   height: 'auto',
   keepSource: true,
   pagerConfig: {},
   proxyConfig: {
     ajax: {
-      query: async ({ page }, formValues) => {
-        message.success(`Query params: ${JSON.stringify(formValues)}`);
+      query: async ({ page }) => {
         return await getExampleTableApi({
           page: page.currentPage,
           pageSize: page.pageSize,
-          ...formValues,
         });
       },
     },
@@ -105,6 +161,19 @@ const [Grid] = useVbenVxeGrid({ formOptions, gridOptions });
 
 <template>
   <Page auto-content-height>
-    <Grid />
+    <Grid>
+      <template #image-url="{ row }">
+        <Image :src="row.imageUrl" height="30" width="30" />
+      </template>
+      <template #open="{ row }">
+        <Switch v-model:checked="row.open" />
+      </template>
+      <template #status="{ row }">
+        <Tag :color="row.color">{{ row.status }}</Tag>
+      </template>
+      <template #action>
+        <Button type="link">编辑</Button>
+      </template>
+    </Grid>
   </Page>
 </template>
